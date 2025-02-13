4822bb45d7c960153d801139109a4be8bdf846f9
cuban.quant <cuban.quant@gmail.com>
2025-02-12 21:20:15 -0500

    New additions to week2 example

--- sample_week2.py
+++ sample_week2.py
@@ -23,7 +23,34 @@ def processData(file_content):
     takes the contents of the file as the first parameter, processes
     the file line by line, and returns a dictionary that maps a person’s ID to a tuple of the form (name, birthday).
     """
-    pass
+    file_lines = file_content.split("\n")
+
+    result_dict = dict()
+    for line in file_lines:
+        # deal with empty line
+        if len(line) == 0:
+            continue
+        # deal with the header
+        if line.startswith("id"):
+            continue
+        
+        elements = line.split(",")
+        id = elements[0]
+        name = elements[1]
+        birthday_string = elements[2]
+        try:
+            # convert the birthday string to a date in format %d/%m/%Y
+            birthday = datetime.datetime.strptime(birthday_string, "%d/%m/%Y")
+            print(f"id = {id} | name = {name} | birthday = {birthday}")
+            # add values to result_dict using id as key and the value will be (name, birthday)
+        except Exception as e:
+            # use the logging module to log error
+            print(e)
+            print(f"there was a problem with line {line}")
+
+        # convert the birthday string to a date in format %d/%m/%Y
+        
+    return result_dict
 
 
 def displayPerson(id, personData):
@@ -37,7 +64,7 @@ def main(url):
     person_dict = processData(url_data)
     
     # ask user for an ID and call displayPerson
-    id = 5
+    id = input("Enter an Id to search: ")
     displayPerson(id, person_dict)
     
 
